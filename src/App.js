afaf05e2557c1f33693a22218ff6b88627556794
OM Miles <miles@ommiles.com>
2021-08-20 16:53:56 -0700

    add toggle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useContext, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ColorContext } from './ColorContext';
 
 function App() {
+  const colors = useContext(ColorContext);
+  const [background, setBackground] = useState(true);
+
   return (
-    <div className='App' id='container'>
+    <div
+      className='App'
+      id='container'
+      style={{
+        backgroundColor: background === true ? colors.blue : colors.red,
+      }}
+    >
+      <button onClick={() => setBackground(!background)}>
+        {background === true ? (
+          <img alt='Light Mode Icon'></img>
+        ) : (
+          <img alt='Dark Mode Icon'></img>
+        )}
+      </button>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
